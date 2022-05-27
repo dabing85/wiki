c8a3433b32d4fc3f7bb35ab8369f366d72bbd48d
hexb3 <hexb3@asiainfo.com>
2022-05-27 23:32:35 +0800

    project init 项目初始化

---
+++ src/test/java/com/dabing/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dabing.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
