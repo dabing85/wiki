1743168f96fa963e04da2a7a7824a2b0111c07f5
hedabing <2261617090@qq.com>
2022-05-29 00:26:29 +0800

    1.6 增加自定义配置项示例

--- src/main/java/com/dabing/wiki/controller/TestController.java
+++ src/main/java/com/dabing/wiki/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.dabing.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello:Test}")
+    private String hello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello,dabing!";
+        return "Hello,dabing!"+hello;
     }
 }
