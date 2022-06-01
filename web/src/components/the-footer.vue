fc489565208c872ba608542fea3aff5dfd5704fe
hedabing <2261617090@qq.com>
2022-06-01 23:41:26 +0800

    3.5 自定义the-header和the-footer组件 并在App.vue中使用

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki ReadBook ©2022 Created by Dabing
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
