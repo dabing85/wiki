63a25a819a2f9392191f27129efe54a7c4d941ae
hedabing <2261617090@qq.com>
2022-06-04 15:34:15 +0800

    4.4 加入Ant Design Vue的list列表组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,23 +48,67 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ebooks}}
-{{ebooks2}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component :is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent ,onMounted, reactive, ref, toRef} from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import axios from "axios";
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  components:{  //引入收藏、点赞、留言图标
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
   },
   setup(){
     console.log("setup");
@@ -73,6 +117,7 @@ export default defineComponent({
       books:[]
     });
 
+    //页面渲染后数据加载
     onMounted(()=>{
       console.log("onMounted");
       axios.get("http://localhost:8081/ebook/list?name=Spring").then((response)=>{
@@ -81,11 +126,30 @@ export default defineComponent({
         ebooks1.books = date.date;
         console.log(response);
       });
-    })
+    });
 
+    //分页
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+
+    //收藏数、点赞数、留言数
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
+
+    //将数据返回
     return{
       ebooks,
-      ebooks2: toRef(ebooks1,"books")
+      ebooks2: toRef(ebooks1,"books"),
+      listData,
+      pagination,
+      actions
     }
 
   },
